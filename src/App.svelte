4f8f0f20da1ae80144a63be609fc8ea5e5702ae4
finnod <finnodonoghue@gmail.com>
2022-10-02 22:34:20 +1100

    resizing using requestAnimationFrame

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,49 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
 	import { Canvas } from "@threlte/core";
 	import Scene from "./Scene.svelte";
 	import NetworkDrawer from "./lib/NetworkDrawer.svelte";
 	import MenuButton from "./lib/MenuButton.svelte";
 	import { showMenu } from "./stores";
 
+	let clientWidth, clientHeight;
 	let cW, cH;
+
+	let isTransitioning = false;
+	let canvas, wrapper;
+	let handler: ReturnType<typeof requestAnimationFrame> | undefined = undefined
+	function render(){
+		// get avaialble width and height and set the canvas size
+		wrapper = document.getElementsByClassName("drawerWrapper")[0];
+		canvas = document.getElementsByClassName("canvas")[0];
+		console.log(canvas.getBoundingClientRect());
+		let rect = canvas.getBoundingClientRect()
+		cW = rect.width;
+		cH = rect.height;
+
+		canvas.setAttribute("style","width:100px");
+		handler = requestAnimationFrame(render);
+	}
+
+	$: if (isTransitioning) {
+		render()
+	} else {
+		if (handler) {
+		cancelAnimationFrame(handler)
+		}
+	}
+
+	onMount(async () => {
+		wrapper = document.getElementsByClassName("drawerWrapper")[0];
+		canvas = document.getElementsByClassName("canvas")[0];
+		wrapper.addEventListener("transitionstart", (e) => (isTransitioning = true));
+		wrapper.addEventListener("transitionend", (e) => (isTransitioning = false));
+		clientWidth = cW;
+		clientHeight = cH;
+	});
+	
+
+	// 
 </script>
 
 <MenuButton />
@@ -13,8 +51,9 @@
 	<div class="drawerWrapper {$showMenu ? 'active' : ''}">
 		<NetworkDrawer />
 	</div>
-
-	<div class="canvas" bind:clientWidth={cW} bind:clientHeight={cH}>
+	
+	
+	<div class="canvas" bind:clientWidth={clientWidth} bind:clientHeight={clientHeight}>
 		<Canvas size={{ width: cW, height: cH }}>
 			<Scene />
 		</Canvas>
@@ -28,14 +67,13 @@
 	}
 
 	.drawerWrapper {
-		background: #444;
 		width: 0px;
 		height: 100vh;
 		transition: width 0.3s;
 	}
 
 	.drawerWrapper.active {
-		width: 20%;
+		width: min(25%, 260px);
 	}
 
 	.canvas {
